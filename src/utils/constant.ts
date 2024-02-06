086a1d2faadbaa2371fbf227f4a3de133ce009cf
Dani2202 <daniyalq2202@gmail.com>
2024-02-06 20:53:18 +0500

    constant added

---
+++ src/utils/constant.ts
@@ -0,0 +1,7 @@
+export const textLinks = [
+    { title: "Features", id: "#" },
+    { title: "How it works", id: "#" },
+    { title: "Pricing", id: "#" },
+    { title: "FAQ", id: "#" },
+    { title: "Contact Us", url: "/contact-us" },
+  ];
