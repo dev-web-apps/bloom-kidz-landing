c355818306d4c833b18e71eadf5e162e8696e54b
Dani2202 <daniyalq2202@gmail.com>
2024-02-07 15:09:20 +0500

    code improved of hero section

--- src/pages/home/hero-section.tsx
+++ src/pages/home/hero-section.tsx
@@ -1,36 +1,45 @@
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Box, Container, SxProps, Typography } from "@mui/material";
 import Banner from "../../assets/home-banner.png";
+import { common } from "@mui/material/colors";
+import { Button } from "../../components";
+
+const boxStyle: SxProps = {
+  height: "100vh",
+  background: "linear-gradient(to bottom, #34A6B1 0%, #1F96C0 100%)",
+  color: common.white,
+  pt: "90px",
+};
+
+const leftBox: SxProps = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: { xs: "column-reverse", md: "row" },
+  width: "100%",
+  pt: 10,
+};
+
+const textStyle: SxProps = {
+  display: "flex",
+  flexDirection: "column",
+  flex: "1",
+  gap: "25px",
+  p: { xs: 3, sm: 7, md: "0px" },
+};
+
+const imgBox: SxProps = {
+  flex: "1",
+  justifySelf: "flex-end",
+  display: "flex",
+  width: { xs: "80%", sm: "90%", md: "100%" },
+};
 
 const HeroSection = () => {
   return (
-    <Box
-      sx={{
-        height: "100vh",
-        background: "linear-gradient(to bottom, #34A6B1 0%, #1F96C0 100%)",
-        color: "white",
-        pt: "90px",
-      }}
-    >
+    <Box sx={boxStyle}>
       <Container maxWidth="lg">
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: { xs: "column-reverse", md: "row" },
-            width: "100%",
-            pt: 10,
-          }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              flex: "1",
-              gap: "25px",
-              p: { xs: 3, sm: 7, md: "0px" },
-            }}
-          >
+        <Box sx={leftBox}>
+          <Box sx={textStyle}>
             <Typography
               fontWeight={600}
               fontSize={{ xs: 38, sm: 48, md: 58 }}
@@ -44,41 +53,19 @@ const HeroSection = () => {
               families in shaping the future of our little ones.
             </Typography>
             <Box display={"flex"} gap={"20px"}>
-              <Button
-                variant="contained"
-                sx={{ p: { xs: "10px", sm: "13px 35px" }, borderRadius: "6px" }}
-              >
-                Start Free Trial
-              </Button>
-              <Button
-                variant="outlined"
-                color="inherit"
-                sx={{
-                  p: { xs: "10px", sm: "13px 35px" },
-                  color: "white",
-                  borderRadius: "6px",
-                }}
-              >
+              <Button variant="contained">Start Free Trial</Button>
+              <Button variant="outlined" color="inherit">
                 See pricing
               </Button>
             </Box>
           </Box>
-          <Box
-            sx={{
-              flex: "1",
-              justifySelf: "flex-end",
-              display: "flex",
-              width: { xs: "80%", sm: "90%", md: "100%" },
-            }}
-          >
+          <Box sx={imgBox}>
             <img
               src={Banner}
               alt=""
               style={{
                 width: "100%",
                 height: "auto",
-                marginLeft: "auto",
-                justifySelf: "flex-end",
               }}
             />
           </Box>
