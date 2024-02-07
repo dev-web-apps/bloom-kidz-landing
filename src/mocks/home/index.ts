8731e1d58fee909a7a61144be8539a07b7420bde
Dani2202 <daniyalq2202@gmail.com>
2024-02-07 19:50:22 +0500

    featureData mock created

---
+++ src/mocks/home/index.ts
@@ -0,0 +1,34 @@
+import AdminIcon from "../../assets/admin-icon.png";
+import FinanceIcon from "../../assets/finance-icon.png";
+
+export const featureData = [
+  {
+    icon: AdminIcon,
+    subText: "Engage parents seamlessly for unified child growth and learning.",
+    heading: "Parents Partnership",
+  },
+  {
+    icon: AdminIcon,
+    subText:
+      "Nurture early development through personalized and enriching educational experiences.",
+    heading: "Child Development",
+  },
+  {
+    icon: FinanceIcon,
+    subText:
+      "Streamline financial processes for swift and hassle-free transactions.",
+    heading: "Quick Finances",
+  },
+  {
+    icon: AdminIcon,
+    subText:
+      "Simplify administrative tasks with an intuitive and user-friendly interface.",
+    heading: "Easy Admin",
+  },
+  {
+    icon: AdminIcon,
+    subText:
+      "Optimize staffing processes for a dynamic and effective early education team.",
+    heading: "Efficient Staffing",
+  },
+];
