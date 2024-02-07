5abcd67d14a5fc81f55ae702b81568631777c61d
Dani2202 <daniyalq2202@gmail.com>
2024-02-07 15:09:36 +0500

    hamburger code seperated

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,21 +1,18 @@
 import {
   Box,
-  ListItem,
   Stack,
   SxProps,
-  Drawer,
-  List,
   IconButton,
   Container,
   Link,
   Button,
 } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
 import Logo from "../../assets/kidzbloom.png";
 import { useNavigate } from "react-router-dom";
 import { textLinks } from "../../utils/constant";
+import Hamburger from "./hamburger";
 
 const wrapper: SxProps = {
   background: "rgba(0, 0, 0, 0.0)",
@@ -50,20 +47,6 @@ const drawerBtn: SxProps = {
     md: "none",
     xs: "flex",
   },
-};
-const closeBtn: SxProps = {
-  color: "white",
-  fontSize: "30px",
-};
-const drawerItems: SxProps = {
-  borderBottom: "1px solid #4b4040",
-  padding: "15px 20px",
-  display: "block",
-  textAlign: "center",
-};
-const drawerButtonWrapper: SxProps = {
-  display: "flex",
-  flexDirection: "column",
 };
 
 export default function Header() {
@@ -155,76 +138,10 @@ export default function Header() {
                 }}
               />
             </IconButton>
-            <Drawer
-              PaperProps={{
-                sx: {
-                  backgroundColor:
-                    "linear-gradient(to bottom, #34A6B1 0%, #1F96C0 100%)",
-                  width: "100%",
-                },
-              }}
-              anchor="right"
-              open={isDrawerOpen}
-              onClose={() => setIsDrawerOpen(false)}
-            >
-              <Box
-                sx={{
-                  background:
-                    "linear-gradient(to bottom, #34A6B1 0%, #1F96C0 100%)",
-                  width: "100%",
-                  height: "100%",
-                }}
-              >
-                <Box sx={{ alignSelf: "end", width: "100%" }}>
-                  <IconButton
-                    sx={{
-                      margin: "12px 16px 5px 0px",
-                      alignSelf: "flex-end",
-                    }}
-                    size="medium"
-                    onClick={toggleDrawer}
-                  >
-                    <CloseIcon sx={closeBtn} />
-                  </IconButton>
-                </Box>
-                <Box>
-                  <List>
-                    {textLinks.map((item, index) => (
-                      <ListItem
-                        sx={drawerItems}
-                        onClick={() => setIsDrawerOpen(false)}
-                        key={index}
-                      >
-                        <Button
-                          variant="text"
-                          color="primary"
-                          key={item.title}
-                          onClick={() => {
-                            item.url
-                              ? navigate(item.url)
-                              : scrollToSection(item.id as string);
-                          }}
-                        >
-                          {item.title}
-                        </Button>
-                      </ListItem>
-                    ))}
-                  </List>
-                  <List sx={drawerButtonWrapper}>
-                    <Button variant="text">Login</Button>
-                    <Button
-                      variant="outlined"
-                      sx={{
-                        width: "200px",
-                        alignSelf: "center",
-                      }}
-                    >
-                      Request Demo
-                    </Button>
-                  </List>
-                </Box>
-              </Box>
-            </Drawer>
+            <Hamburger
+              isDrawerOpen={isDrawerOpen}
+              setIsDrawerOpen={setIsDrawerOpen}
+            />
           </Stack>
         </Container>
       </Box>
