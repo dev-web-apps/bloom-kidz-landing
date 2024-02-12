97da1a6055e434f04c89bcb2eb78e3e84253acc4
Muhammad-Huzaifa15180 <mh335130@gmail.com>
2024-02-12 21:33:15 +0500

    created mock data

---
+++ src/mocks/contact us/index.tsx
@@ -0,0 +1,27 @@
+export const Accordiondata=[
+    {   id: 1,
+        heading:"How does Bloom Kidz work?",
+        subheading:"All things starts with a homepage —  Get inspired without breaking your wallet with premium Figma and Sketch templates. Capitalize hanging fruit to identify a ballpark value added activity to beta test. Podcasting operational — change management inside of workflows."
+    },
+    {   
+        id: 2,
+        heading:"Supported platforms",
+        subheading:"All things starts with a homepage —  Get inspired without breaking your wallet with premium Figma and Sketch templates. Capitalize hanging fruit to identify a ballpark value added activity to beta test. Podcasting operational — change management inside of workflows."
+    },
+    {   
+        id: 3,
+        heading:"Payment methods",
+        subheading:"All things starts with a homepage —  Get inspired without breaking your wallet with premium Figma and Sketch templates. Capitalize hanging fruit to identify a ballpark value added activity to beta test. Podcasting operational — change management inside of workflows."
+    },
+    {   
+        id: 4,
+        heading:"Help and support",
+        subheading:"All things starts with a homepage —  Get inspired without breaking your wallet with premium Figma and Sketch templates. Capitalize hanging fruit to identify a ballpark value added activity to beta test. Podcasting operational — change management inside of workflows."
+    },
+    {
+        id: 5,
+        heading:"Data and statistics",
+        subheading:"All things starts with a homepage —  Get inspired without breaking your wallet with premium Figma and Sketch templates. Capitalize hanging fruit to identify a ballpark value added activity to beta test. Podcasting operational — change management inside of workflows."
+    },
+
+]
