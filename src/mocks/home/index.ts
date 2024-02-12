c7861cf5a54f82936141e8f7b2a9fc3f88aa74aa
Dani2202 <daniyalq2202@gmail.com>
2024-02-12 16:10:26 +0500

    mock added

--- src/mocks/home/index.ts
+++ src/mocks/home/index.ts
@@ -32,3 +32,19 @@ export const featureData = [
     heading: "Efficient Staffing",
   },
 ];
+
+export const featureMock = {
+  Heading1: "Transforming Early Education Together",
+  SubText1:
+    "Unlock the full potential of early childhood education with Bloom Kidz, a comprehensive platform designed to elevate collaboration, streamline processes, and foster a nurturing environment for every child.",
+  Heading2: "Designed with You in Mind",
+  SubText2:
+    "Discover an innovative platform that prioritizes your needs, offering a seamless and enriching experience for both educators and parents.",
+  SubText2More:
+    "From effortless navigation to dedicated support, Bloom Kidz is here to transform early education with you at the center.",
+  SubText2Array: [
+    "User Friendly",
+    "Free Training",
+    "Help is Just a Click Away",
+  ],
+};
