062b434eabd018f96d9bbd8ab0b1fa92595eccfa
Dani2202 <daniyalq2202@gmail.com>
2024-02-12 21:26:13 +0500

    packges added

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "./hero-section";
 import KeyFeatures from "./key-features";
+import Packages from "./packages";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <KeyFeatures id="features" />
+      <Packages />
     </>
   );
 };
