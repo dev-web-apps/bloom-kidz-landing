57f6964dfadad22a4c3ee4e6f9dbc2f95947609c
Dani2202 <daniyalq2202@gmail.com>
2024-03-05 19:54:56 +0500

    startFreeTrial and getAllPackages services added

--- src/services/api.services.ts
+++ src/services/api.services.ts
@@ -12,3 +12,27 @@ export function requestDemo(body: IRequestDemo): Promise<AxiosResponse> {
       });
   });
 }
+
+export function getAllPackages(): Promise<AxiosResponse> {
+  return new Promise((resolve, reject) => {
+    http("/package", "get", null, null)
+      .then((res: AxiosResponse) => {
+        resolve(res);
+      })
+      .catch((err: AxiosError) => {
+        reject(err);
+      });
+  });
+}
+
+export function startFreeTrial(body: ISignUp): Promise<AxiosResponse> {
+  return new Promise((resolve, reject) => {
+    http("/user/create-free-trial-user", "post", body, null)
+      .then((res: AxiosResponse) => {
+        resolve(res);
+      })
+      .catch((err: AxiosError) => {
+        reject(err);
+      });
+  });
+}
